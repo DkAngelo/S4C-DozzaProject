3b39aad6dfff6c28a7eaadf741dce69919a0a6ff
DkAngelo <angelochiacchio03@gmail.com>
2025-10-10 17:36:23 +0200

    added italian shapefiles, added graphs images, minor updates

--- data_cleansing/data_cleansing.py
+++ data_cleansing/data_cleansing.py
@@ -4,8 +4,8 @@ from data_clensing1 import clean_data
 
 engine = create_engine("postgresql://admin:admin123@localhost:5432/DozzaDB")
 
-folder_path = "/home/angelo/Downloads/dozza_PresenzeTuristi_202508" #inserire la cartella di UN SOLO mese
-file_type = "PRESENZE" #inserire PRESENZE o SPOSTAMENTI in base al tipo di dati da gestire
+folder_path = "" #inserire la cartella di UN SOLO mese
+file_type = "" #inserire PRESENZE o SPOSTAMENTI in base al tipo di dati da gestire
 
 items = os.listdir(folder_path)
 clean_data(engine, items, folder_path, file_type)
